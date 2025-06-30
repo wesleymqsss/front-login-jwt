1b8409c055a2f94e334e891895fbb9ee01c6a6f5
Wesley Marques <wesleymqsss@gmail.com>
2025-06-29 23:02:18 -0300

    tratativa para verificar se existe token no local storage antes de requisitar o backend

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -1,20 +1,21 @@
 import { HttpContextToken, HttpEvent, HttpHandlerFn, HttpRequest } from "@angular/common/http";
-import { Observable } from "rxjs";
+import { catchError, Observable, throwError } from "rxjs";
 
 export const AUTH_TOKEN_ENABLED = new HttpContextToken<boolean>(() => true);
 
 export function authInterceptor(
     req: HttpRequest<unknown>, 
     next: HttpHandlerFn): Observable<HttpEvent<unknown>>{
-        let newReq = req;
-        console.log('token header adicao?', AUTH_TOKEN_ENABLED)
         const APLLY_AUTH_TOKEN = req.context.get(AUTH_TOKEN_ENABLED);
-        if(APLLY_AUTH_TOKEN){
-            newReq = req.clone({
-                headers: req.headers.set('Authorization', `Bearer ${localStorage.getItem('token')}`)
-            });
-            return next(newReq);
+        const TOKEN = APLLY_AUTH_TOKEN ? localStorage.getItem('token') : null;
+        
+        if(APLLY_AUTH_TOKEN && !TOKEN){
+            return throwError(() => new Error('Token nao econtrado.'));
         }
+
+        const newReq = TOKEN ? req.clone({
+            headers: req.headers.set('Authorization', `Bearer ${localStorage.getItem('token')} `)
+        }) : req;
 
         return next(newReq);
 }
